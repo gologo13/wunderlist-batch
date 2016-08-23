1027e68701478dac993c90235b78ca5f554ffd22
Yohei Yamaguchi <joker13meister@gmail.com>
2016-08-24 01:24:27 +0900

    Support index input on create.

--- commands/create.js
+++ commands/create.js
@@ -10,6 +10,7 @@ module.exports = class CreateCommand {
 
     const configuration = new Configuration();
     configuration.load();
+
     this.wunderListClient = new WunderlistSdk({
       accessToken: configuration.accessToken,
       clientID: configuration.clientId
@@ -17,34 +18,41 @@ module.exports = class CreateCommand {
   }
 
   execute() {
-    // FIX ME:use Promise like below.
-    // const wlbatch = new WlBatch({at, clientID});
-    // wlbatch.listUpLists().acceptUserInput().addTask();
-
+    // FIX ME: refactor with Promise.
     this.wunderListClient.http.lists.all()
       .done(lists => {
-        console.log("Select one of indexes below to add a recurrence task");
+        var indexHash = {};
+
+        console.log("You have the followings lists in Wunderlist.");
+
         lists.forEach((list, index) => {
           console.log("%d: %s(%d)", index, list.title, list.id);
+          indexHash[index] = list.id;
         });
 
-        console.log("> ");
+        console.log("");
+        console.log("Select one of indexes to add a recurrence task");
+
+        process.stdout.write("> ");
 
         process.stdin.resume();
         process.stdin.setEncoding('utf8');
 
-        var selectedListId = '';
+        var selectedIndex = '';
         process.stdin.on('data', chunk => {
-          selectedListId += chunk;
+          selectedIndex += chunk;
         });
 
         process.stdin.on('end', () => {
+          // FIX ME: handle out of index.
+
           // There will be a trailing \n from the user hitting enter. Get rid of it.
-          selectedListId = selectedListId.replace(/\n$/, '');
-          console.log("selectedListId: %s", selectedListId);
+          selectedIndex = selectedIndex.replace(/\n$/, '');
+          console.log("selectedIndex: %s", selectedIndex);
+          console.log("selectedListId: %s", indexHash[selectedIndex]);
 
           const obj = {
-            list_id: parseInt(selectedListId, 10),           // eslint-disable-line camelcase
+            list_id: parseInt(indexHash[selectedIndex], 10),           // eslint-disable-line camelcase
             title: "WLBatch test"
           };
           console.log(obj);
